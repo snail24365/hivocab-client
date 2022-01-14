a36defa13c29b338b20cd1517f9604c79f0bd2eb
Seunghui Choi <snail24365@gmail.com>
2022-01-15 02:36:52 +0900

    Feat: WordList 및Report 페이지 프레임 구성

--- src/component/Report.jsx
+++ src/component/Report.jsx
@@ -4,6 +4,32 @@ const Report = () => {
   return (
     <>
       <div>레포트</div>
+      <div>오늘 목표 분량</div>
+      <div>50</div>
+      <div>오늘 공부한 분량</div>
+      <div>14</div>
+      <div>Progress Indicator</div>
+      <div>출석표</div>
+      <table>
+        <tr>
+          <td>월</td>
+          <td>화</td>
+          <td>수</td>
+          <td>목</td>
+          <td>금</td>
+          <td>토</td>
+          <td>일</td>
+        </tr>
+        <tr>
+          <td>o</td>
+          <td>o</td>
+          <td>o</td>
+          <td>x</td>
+          <td>x</td>
+          <td>o</td>
+          <td>x</td>
+        </tr>
+      </table>
     </>
   );
 };
--- src/component/WordList.jsx
+++ src/component/WordList.jsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+function fetchWordInfos() {
+  return [...Array(10).keys()].map((x) => {
+    return {
+      id: x,
+      word: "Apple",
+      usages: [
+        { id: 0, description: "description1" },
+        { id: 1, description: "description2" },
+      ],
+    };
+  });
+}
+
+function fetchMaxPageNumber() {
+  return fetchWordInfos().length;
+}
 
 const WordList = () => {
+  const [wordInfos, setWordInfos] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  var maxPageNumber = 9999999;
+  useEffect(() => {
+    setWordInfos(fetchWordInfos());
+    maxPageNumber = fetchMaxPageNumber();
+  }, []);
+
+  const movePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const moveNextPage = () => {
+    if (currentPage < maxPageNumber) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const wordList = wordInfos.map((wordInfo) => (
+    <li>
+      <div>{wordInfo.word}</div>
+      <div>
+        <ul>
+          {wordInfo.usages.map((usage) => (
+            <li>{usage.description}</li>
+          ))}
+        </ul>
+      </div>
+    </li>
+  ));
+
+  const pageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((x) => (
+    <li key={x} className={`${x === currentPage ? "selected" : ""}`}>
+      {x}
+    </li>
+  ));
+  pageIndexes.push(<li key="dots"> ... </li>);
+
   return (
     <>
       <div>단어 리스트</div>
+      <ul>{wordList}</ul>
+      <div>
+        <button onClick={movePreviousPage}>left</button>
+        <ul>{pageIndexes}</ul>
+        <button onClick={moveNextPage}>right</button>
+      </div>
     </>
   );
 };
