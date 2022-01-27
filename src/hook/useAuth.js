0e03135ed36ad4d0c611e4d5bc69a5572aecf659
Seunghui Choi <snail24365@gmail.com>
2022-01-27 09:56:25 +0900

    Feat: 로그인 실패 로직 수정

--- src/hook/useAuth.js
+++ src/hook/useAuth.js
@@ -24,21 +24,14 @@ const useAuth = () => {
         axios.post(SERVER + "/login", JSON.stringify(payload))
           .then(response => {
             console.log("ERROR" + response)
-            if (response.status === 204) {
-
-            }
             if (response.status === 200) {
               setIsAuthenticated(true);
               setCookie("Authorization", 'Bearer ' + response.data["access_token"], 60 * 24);
               window.location.href = "/";
             }
-            else {
-              alert("아이디, 패스워드가 일치하지 않습니다.")
-            }
           })
           .catch(e => {
-            alert("서버 에러가 발생했습니다. 잠시 후 다시 시도해주십시오.")
-            console.log(e)
+            alert("아이디, 패스워드가 일치하지 않습니다.")
           })
         break;
 
